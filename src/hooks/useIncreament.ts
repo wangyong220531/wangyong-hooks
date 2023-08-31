1d437c08f4a71bd1029ff4414d2d87f8b4e90aab
王勇 <wy19945372694@163.com>
2023-08-31 10:37:23 +0800

    20231033 08:37 update

---
+++ src/hooks/useIncreament.ts
@@ -0,0 +1,39 @@
+import { useState, useEffect, useRef } from "react"
+
+const useIncreament = (startValue: number, endValue: number): number => {
+    const [value, setValue] = useState(startValue)
+    const duration = (endValue - startValue) * 1000
+    const startTimeRef = useRef<number | null>(null)
+    const animationFrameRef = useRef<number | null>(null)
+
+    useEffect(() => {
+        const startAnimation = (timestamp: number) => {
+            if (!startTimeRef.current) {
+                startTimeRef.current = timestamp
+            }
+            
+            const elapsedTime = timestamp - startTimeRef.current
+            const progress = elapsedTime / duration
+            const diff = endValue - startValue
+            const newValue = startValue + Math.floor(diff * progress)
+
+            setValue(newValue)
+
+            if (progress < 1) {
+                animationFrameRef.current = requestAnimationFrame(startAnimation)
+            }
+        }
+
+        animationFrameRef.current = requestAnimationFrame(startAnimation)
+
+        return () => {
+            if (animationFrameRef.current) {
+                cancelAnimationFrame(animationFrameRef.current)
+            }
+        }
+    }, [startValue])
+
+    return value
+}
+
+export default useIncreament
