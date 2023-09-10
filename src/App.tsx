227dd4ccfd870f5428c124e80e673e121981078d
wangyong220531 <wangyong220531@gmail.com>
2023-09-11 00:18:03 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import useIncreament from "./hooks/useIncreament"
 
 function App() {
-    const value = useIncreament(0, 10)
+    const [value, changeValue] = useIncreament(200, 3000);
 
-    return <>{value}</>
+    const handleClick = () => {
+        changeValue(10);
+    };
+
+    return (
+        <div>
+            <p>{value}</p>
+            <button onClick={handleClick}>改变值为10</button>
+        </div>
+    );
 }
 
 export default App
--- src/hooks/useIncreament.ts
+++ src/hooks/useIncreament.ts
@@ -1,10 +1,12 @@
 import { useState, useEffect, useRef } from "react"
 
-const useIncreament = (startValue: number, endValue: number): number => {
-    const [value, setValue] = useState(startValue)
-    const duration = (endValue - startValue) * 1000
+const useIncreament = (initialTargetValue: number, duration: number): [number, (e: number) => void] => {
+    const [value, setValue] = useState(0)
+    const targetValueRef = useRef(initialTargetValue)
     const startTimeRef = useRef<number | null>(null)
     const animationFrameRef = useRef<number | null>(null)
+    const startValueRef = useRef<number>(0)
+    const [isIt, setIsIt] = useState(false)
 
     useEffect(() => {
         const startAnimation = (timestamp: number) => {
@@ -14,26 +16,46 @@ const useIncreament = (startValue: number, endValue: number): number => {
 
             const elapsedTime = timestamp - startTimeRef.current
             const progress = elapsedTime / duration
-            const diff = endValue - startValue
-            const newValue = startValue + Math.floor(diff * progress)
+            const diff = targetValueRef.current - startValueRef.current
+            const newValue = Math.floor(startValueRef.current + diff * progress)
 
             setValue(newValue)
 
-            if (progress < 1) {
+            if (Math.floor(progress) < 1) {
                 animationFrameRef.current = requestAnimationFrame(startAnimation)
+            } else {
+                setValue(targetValueRef.current)
             }
         }
+
+        if (animationFrameRef.current) {
+            cancelAnimationFrame(animationFrameRef.current)
+        }
+
+        startValueRef.current = value
+
+        if (!isIt) {
+            targetValueRef.current = initialTargetValue
+        }
+
+        startTimeRef.current = null
 
         animationFrameRef.current = requestAnimationFrame(startAnimation)
 
         return () => {
             if (animationFrameRef.current) {
                 cancelAnimationFrame(animationFrameRef.current)
+                setIsIt(false)
             }
         }
-    }, [startValue])
+    }, [targetValueRef.current])
 
-    return value
+    const changeValue = (e: number) => {
+        targetValueRef.current = e
+        setIsIt(true)
+    }
+
+    return [value, changeValue]
 }
 
 export default useIncreament
